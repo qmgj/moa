91aa89bc1a64f3be4680c110a8d03f7237785b20
qmgj <qmgj@msn.com>
2012-06-05 15:42:48 +0800

    phonegap

--- src/com/rikee/moa/MoaActivity.java
+++ src/com/rikee/moa/MoaActivity.java
@@ -1,13 +1,14 @@
 package com.rikee.moa;
 
-import android.app.Activity;
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
 
-public class MoaActivity extends Activity {
+public class MoaActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
